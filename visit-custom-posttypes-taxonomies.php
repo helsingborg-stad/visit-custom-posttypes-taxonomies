ce6132dd6ef2a15cd24bb6ff95b28a2b8a3aa82e
GitHub Action <>
2025-01-08 12:29:27 +0000

    build: update package file versions to 2.0.5

--- visit-custom-posttypes-taxonomies.php
+++ visit-custom-posttypes-taxonomies.php
@@ -4,7 +4,7 @@
  * Plugin Name: Visit Helsingborg: Custom Post Types, Taxonomies and ACF Fields
  * Plugin URI: -
  * Description:
- * Version: 2.0.4
+ * Version: 2.0.5
  * Author: Anna Johansson
  * Author URI: -
  * Text domain: visit
